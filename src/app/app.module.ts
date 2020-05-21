0679fb1819c3f5c695ec438b00cb3d3706d05b2c
Hashiix <narusuke94@gmail.com>
2020-05-22 01:07:10 +0200

    folder fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthButtonComponent
   ],
   imports: [
     BrowserModule
