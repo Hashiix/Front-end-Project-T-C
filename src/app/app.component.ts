2fd5c0aea44ba414120f1961919d34768341361e
Deyris <deyris.raza@gmail.com>
2020-05-27 16:15:46 +0200

    Page Profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { MustMatch } from './_helpers/must-match.validator';;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'front-end-project';
+
+  constructor() {
+
+  }
+
+  ngOnInit ()  {
+    
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: AppComponent, },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  exports: [
+    RouterModule
+  ],
+  providers: [ProfileComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
