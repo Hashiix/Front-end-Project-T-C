b2b2437b5ba065a8681b5c10f06528e6cd73f2d6
Hashiix <narusuke94@gmail.com>
2020-05-28 09:13:50 +0200

    add homeOffline module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeOfflineComponent } from './modules/home-offline/home-offline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeOfflineComponent
   ],
   imports: [
     BrowserModule
