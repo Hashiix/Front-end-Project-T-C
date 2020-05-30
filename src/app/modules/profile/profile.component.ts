c5ef3048e1bc45238970c618942509059796b5c1
Deyris <deyris.raza@gmail.com>
2020-05-30 20:33:14 +0200

    Pseudo finally Required ! (FORM)

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,21 +7,28 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-      profileForm = this.fb.group({
-      name: ['', Validators.required],
-      status: [''],
-      password: [''],
-      });
+      profileForm = new FormGroup({});
+      submitted = false;
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-    
+  ngOnInit() {
+     this.profileForm = this.fb.group({
+        name : ['', Validators.required],
+        status: [''],
+        password: ['']
+     });
   }
 
   onSubmit() {
     
-    alert("Vos modifications ont bien été enregistrées !")
+    this.submitted = true;
+
+    if (this.profileForm.invalid) {
+      return;
+  } else {
+    alert("Vos modifications ont bien été enregistrées !");
+  }
 
   }
 
